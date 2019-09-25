0d16f809df2f267c3c349654cc64d2cab5c41ae6
neohed <dave@neohed.com>
2019-09-25 21:28:19 +0100

    Initial commit

---
+++ MakeMosaic/Program.cs
@@ -0,0 +1,121 @@
+﻿using ImageProcessor;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace MakeMosaic
+{
+    class Program
+    {
+        const string imageFolder = @"..\..\Images\";
+        const int pixelateSize = 10;
+        const string pixelatedImageAppend = "_pix.jpg";
+
+        static void Main(string[] args)
+        {
+            var images = GetImages();
+
+            foreach(string image in images.Where(i => !i.EndsWith(pixelatedImageAppend)))
+            {
+                string fn = Path.GetFileNameWithoutExtension(image);
+
+                var img = LoadImage(image);
+                img.Pixelate(pixelateSize).Save(imageFolder + fn + pixelatedImageAppend);
+                img.Dispose();
+            }
+
+            images = GetImages();
+
+            foreach (string image in images.Where(i => i.EndsWith(pixelatedImageAppend)))
+            {
+                string fn = Path.GetFileNameWithoutExtension(image);
+
+                var img = Image.FromFile(image);
+                var id = GetColours(img);
+                id.Name = fn;
+                string output = JsonConvert.SerializeObject(id);
+                File.WriteAllText(imageFolder + fn + ".json", output);
+            }
+        }
+
+        static string[] GetImages()
+        {
+            string[] fileEntries = Directory.GetFiles(imageFolder, "*.jpg");
+            return fileEntries;
+        }
+
+        static ImageFactory LoadImage(string image)
+        {
+            // Initialize the ImageFactory using the overload to preserve EXIF metadata.
+            ImageFactory imageFactory = new ImageFactory(preserveExifData: true);
+
+            byte[] photoBytes = File.ReadAllBytes(image);
+
+            using (MemoryStream inStream = new MemoryStream(photoBytes))
+            {
+                using (MemoryStream outStream = new MemoryStream())
+                {
+                    imageFactory.Load(inStream);
+
+                    return imageFactory;
+                }
+            }
+        }
+
+        static ImageData GetColours(Image img)
+        {
+            using (Bitmap bmp = new Bitmap(img))
+            {
+                int width = bmp.Width;
+                int height = bmp.Height;
+                List<string> colours = new List<string>();
+
+                for (int y = pixelateSize / 2; y < height; y += pixelateSize)
+                {
+                    for (int x = pixelateSize / 2; x < width; x += pixelateSize)
+                    {
+                        Color clr = bmp.GetPixel(x, y);
+                        int red = clr.R;
+                        int green = clr.G;
+                        int blue = clr.B;
+                        string RGB = red.ToString("X2")
+                            + green.ToString("X2")
+                            + blue.ToString("X2");
+
+                        colours.Add("#"
+                            + ShortRGB(RGB)
+                            );
+                    }
+                }
+
+                ImageData id = new ImageData();
+                id.Colours = colours.ToArray();
+                id.Width = (int)Math.Ceiling((decimal)width / pixelateSize);
+
+                return id;
+            }
+        }
+
+        static string ShortRGB(string rgb)
+        {
+            if (rgb[0] == rgb[1] &&
+                rgb[2] == rgb[3] &&
+                rgb[4] == rgb[5])
+            {
+                return rgb.Substring(0, 1) + rgb.Substring(2, 1) + rgb.Substring(4, 1);
+            }
+
+            return rgb;
+        }
+    }
+
+    internal class ImageData
+    {
+        public string Name { get; set; }
+        public int Width { get; set; }
+        public string[] Colours { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# make-mosaic
+Console app to generate mosaic data from an image.
+
+Generates json data of colours from a pixelated version of any .jpg files in the Images folder.
